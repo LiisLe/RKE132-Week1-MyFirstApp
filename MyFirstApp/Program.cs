daa0f9771b61e6ee806b58c50d8fbd2e2c8b56ec
Liis Lensment <liis.lensment@gmail.com>
2024-03-19 20:44:47 +0200

    add files

---
+++ MyFirstApp/Program.cs
@@ -0,0 +1,16 @@
+﻿// This is a comment to my code
+
+//Rakendus küsib kasutajal sisestada tema nime 
+//rakendus tervitab kasutajat nimepidi
+
+
+Console.WriteLine("Enter your name:"); //Output
+//String - sõne
+String userName; 
+
+ userName=Console.ReadLine(); //Input
+
+Console.WriteLine("Hello" + ", " + userName + "!"); //Output
+
+//String interpolation
+Console.WriteLine($"Hello, {userName}!"); //Output
